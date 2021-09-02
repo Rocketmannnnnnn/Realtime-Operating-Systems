8a4a78b3529ecf6c8fc2d6370a55dbc0bce34d06
Jasper <j.bonefaas@student.avans.nl>
2021-09-02 15:16:29 +0200

    Update main.c

--- Blinking led/CC3220S_leeg_project/main.c
+++ Blinking led/CC3220S_leeg_project/main.c
@@ -6,39 +6,41 @@
 /*
  *  ======== main ========
  */
-int main(void)
+
+//Enable leds throug memory adresses
+void assignment1()
 {
     //Enable clock module GPIOA1
-    //Base adres GPIOA1: ARCM_BASE
-    //Register offset: GPIOCLKEN = APPS_RCM_O_GPIO_B_CLK_GATING
-    HWREG(ARCM_BASE + (APPS_RCM_O_GPIO_B_CLK_GATING)) |= 0x01;
+        //Base adres GPIOA1: ARCM_BASE
+        //Register offset: GPIOCLKEN = APPS_RCM_O_GPIO_B_CLK_GATING
+        HWREG(ARCM_BASE + (APPS_RCM_O_GPIO_B_CLK_GATING)) |= 0x01;
 
-    //Pin multiplexer
-    //Base adres: OCP_SHARED_BASE
-    //Register (GPIO PIN) offset: HWREG(OCP_SHARED_BASE + OCP_SHARED_O_GPIO_PAD_CONFIG_9);
-    HWREG(OCP_SHARED_BASE + OCP_SHARED_O_GPIO_PAD_CONFIG_9) = 0x40;
-    HWREG(OCP_SHARED_BASE + OCP_SHARED_O_GPIO_PAD_CONFIG_10) = 0x40;
-    HWREG(OCP_SHARED_BASE + OCP_SHARED_O_GPIO_PAD_CONFIG_11) = 0x40;
+        //Pin multiplexer
+        //Base adres: OCP_SHARED_BASE
+        //Register (GPIO PIN) offset: HWREG(OCP_SHARED_BASE + OCP_SHARED_O_GPIO_PAD_CONFIG_9);
+        HWREG(OCP_SHARED_BASE + OCP_SHARED_O_GPIO_PAD_CONFIG_9) = 0x40;
+        HWREG(OCP_SHARED_BASE + OCP_SHARED_O_GPIO_PAD_CONFIG_10) = 0x40;
+        HWREG(OCP_SHARED_BASE + OCP_SHARED_O_GPIO_PAD_CONFIG_11) = 0x40;
 
-    int c,d;
-    int led = 0;
+        int c;
+        int led = 0;
 
-    while (1)
-    {
-        for(c = 0; c < 32767; c++)
+        while (1)
         {
-            for(d = 0; d < 200; d++)
-            {
+            for(c = 0; c < 6000000; c++) { }
 
+            if(led > 14){
+                led = 0;
             }
-        }
 
-        if(led > 14){
-            led = 0;
+            HWREG(GPIOA1_BASE + GPIO_O_GPIO_DIR) = led;
+            HWREG(GPIOA1_BASE + (GPIO_O_GPIO_DATA + (led << 2))) = led;
+            led += 2;
         }
+}
 
-        HWREG(GPIOA1_BASE + GPIO_O_GPIO_DIR) = led;
-        HWREG(GPIOA1_BASE + (GPIO_O_GPIO_DATA + (led << 2))) = led;
-        led += 2;
-    }
+
+int main(void)
+{
+    assignment1();
 }
