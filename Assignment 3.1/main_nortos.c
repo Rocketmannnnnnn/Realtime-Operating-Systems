91d30bcf39b2ba48f06bb6a63f3a1acdab33aef9
Jasper <j.bonefaas@student.avans.nl>
2021-09-21 16:33:54 +0200

    Update main_nortos.c

--- Assignment 3.1/main_nortos.c
+++ Assignment 3.1/main_nortos.c
@@ -1,4 +1,3 @@
-
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -10,180 +9,125 @@
 #include <ti/devices/cc32xx/driverlib/systick.h>
 
 //Enums & constants
-enum STATE { waiting, ready, finished };
-enum COLOR { red, yellow, green };
-int periods[3] = { 200, 500, 750 };
-int delays[3] = { 100, 200, 300 };
+enum STATES { waiting, ready };
+enum COLOUR { red, yellow, green };
+int periodTime[3] = { 200, 500, 750 }; //Length of period
+int delayTime[3] = { 100, 200, 300 };
 
 //Structs
 typedef struct {
-    enum STATE state;
-    enum COLOR color;
+    enum STATES state;
+    enum COLOUR color;
     int counter;
     int delay;
     void (*pointerNaarFunc)(int, int);
 } Task;
 
 typedef struct {
-    void *prev;
-    void *next;
+    void* next;
     Task task;
 } TaskHolder;
 
 //Globals
-static TaskHolder *head = NULL;
+static TaskHolder* head = NULL;
 
-//Function defenitions;
-void addTask(enum COLOR color);
-TaskHolder* getTask(int i);
-int taskCount();
+//Function definitions;
+void addTask(enum COLOUR color);
 void runReadyTasks();
 void setLed(int ledIndex, int power);
 
-void INT(){
+void INT() {
     TaskHolder* node;
     node = head;
 
-    while (1)
-    {
-        if (!node)
-            return;
-
-        switch(node->task.state){
-            case waiting:
-                if(node->task.delay <= 0){
-                    node->task.state = ready;
-                }
-                else{
-                    node->task.delay--;
-                }
-                break;
-            case ready:
-                if(node->task.counter <= 0){
-                    node->task.state = finished;
-                }
-                else{
-                    node->task.counter--;
-                }
-                break;
-            default:
-                break;
+    while (node != NULL) {
+        switch (node->task.state) {
+        case waiting:
+            if (node->task.delay <= 0) {
+                node->task.state = ready;
+            }
+            else {
+                node->task.delay--;
+            }
+            break;
+        case ready:
+            if (node->task.counter <= 0) {
+                node->task.state = waiting;
+                node->task.delay = delayTime[node->task.color];
+                node->task.counter = periodTime[node->task.color];
+            }
+            else {
+                node->task.counter--;
+            }
+            break;
+        default:
+            break;
         }
         node = node->next;
     }
 }
 
-void addTask(enum COLOR color){
-    TaskHolder *t;
-    t = (TaskHolder*) malloc(sizeof(TaskHolder));
-    if(head == NULL){
+void addTask(enum COLOUR color) {
+    TaskHolder* t;
+    t = (TaskHolder*)malloc(sizeof(TaskHolder));
+    if (head == NULL) {
         head = t;
         head->next = NULL;
     }
-    else
-    {
+    else {
         t->next = head;
-        head->prev = t;
         head = t;
     }
-    head->prev = NULL;
     head->task.state = waiting;
     head->task.color = color;
-    head->task.delay = delays[color];
-    head->task.counter = periods[color];
+    head->task.delay = delayTime[color];
+    head->task.counter = periodTime[color];
     head->task.pointerNaarFunc = setLed;
 }
 
-TaskHolder* getTask(int i){
-    int iterator = 0;
+void runReadyTasks() {
     TaskHolder* node;
     node = head;
-
-    while (1)
-    {
-        if (!node)
-            return NULL;
-
-        if (iterator == i)
-            return node;
-        else
-            iterator++;
-
-        node = node->next;
-    }
-}
-
-int taskCount(){
-    int iterator = 0;
-    TaskHolder* node;
-    node = head;
-
-    while (1)
-    {
-        if (!node)
-            return iterator;
-
-        node = node->next;
-        iterator++;
-    }
-}
-
-void runReadyTasks(){
-    TaskHolder* node;
-        node = head;
-
-        while (1)
-        {
-            if (!node)
-                return;
-
-            if(node->task.state == ready){
-                node->task.pointerNaarFunc(node->task.color, CONFIG_GPIO_LED_ON);
-            }
-            node = node->next;
+    while (node != NULL) {
+        if (node->task.state == ready) {
+            node->task.pointerNaarFunc(node->task.color, CONFIG_GPIO_LED_ON);
         }
-}
-
-void setLed(int ledIndex, int power){
-    if(ledIndex == 0){
-        GPIO_write(CONFIG_LED_0_GPIO, power);
-    }
-    if(ledIndex == 1){
-        GPIO_write(CONFIG_LED_1_GPIO, power);
-    }
-    if(ledIndex == 2){
-        GPIO_write(CONFIG_LED_2_GPIO, power);
+        node = node->next;
     }
 }
 
+void setLed(int ledIndex, int power) {
+    if (ledIndex == 0) {
+        GPIO_write(CONFIG_GPIO_LED_GREEN, power);
+    }
+    if (ledIndex == 1) {
+        GPIO_write(CONFIG_GPIO_LED_YELLOW, power);
+    }
+    if (ledIndex == 2) {
+        GPIO_write(CONFIG_GPIO_LED_RED, power);
+    }
+}
 
-int main(){
+int main() {
     Board_init();
     NoRTOS_start();
-
     GPIO_init();
 
     //set period. 1 milli second is 80000. Multiply by 1000 and get a second
     SysTickPeriodSet(80000);
-    //enable
-    SysTickEnable();
     //Register interrupt
     SysTickIntRegister(INT);
+    //enable
+    SysTickEnable();
 
     //ADD STRUCTS
     addTask(red);
-    addTask(red);
-    addTask(red);
     addTask(yellow);
-    addTask(yellow);
-    addTask(yellow);
-    addTask(green);
     addTask(green);
 
-    while(1){
+    while (1) {
         //SET ALL LEDS OFF
-        int i;
-        for(i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             setLed(i, CONFIG_GPIO_LED_OFF);
         }
         runReadyTasks();
